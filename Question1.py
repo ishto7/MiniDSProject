6bb668fe9c00033c26a34eb093204433b12e04db
Iman Nazari <imaan.nazarii@gmail.com>
2020-04-03 21:11:26 +0430

    create_p_pool function defined

--- Question1.py
+++ Question1.py
@@ -1,4 +1,16 @@
 from random import shuffle
+import statistics
+
+BIG_N = 10 ** 5
+
+
+def create_p_pool(M, N):
+    card_deck = list(range(M)) * N  # The numbers are suit_ids
+    p_pool = []
+    for _ in range(BIG_N):
+        shuffle(card_deck)
+        p_pool.append(calculate_p(card_deck))
+    return p_pool
 
 
 def calculate_p(deck: list):
@@ -15,6 +27,7 @@ def calculate_p(deck: list):
 if __name__ == '__main__':
     M = 4
     N = 13
-    default_cards = list(range(M)) * N  # The numbers are suit_ids
-    shuffle(default_cards)
-    print("P for a random normal deck is:", calculate_p(default_cards))
+    p_pool = create_p_pool(M, N)
+    p_bar = statistics.mean(p_pool)
+    print("Mean of P:", p_bar)
+    print("STdev of P:", statistics.stdev(p_pool, p_bar))
